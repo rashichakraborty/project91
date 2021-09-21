d2f5f68463f6abaf95f15b1efa8cf18f4e3a9654
rashichakraborty <77398317+rashichakraborty@users.noreply.github.com>
2021-09-21 16:52:57 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,17 @@
+function send()
+{
+    number1 = document.getElementById("number1").Value;
+    number2 = document.getElementById("number2").Value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+
+
+    question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
+    input_box = "<br>Answer : <input types='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_number + input_box + check_button;
+
+
+    document.getElementById("output").innerHTML = row;
+    document.getElementById(number1).Value = "";
+    document.getElementById(number2).Value = "";
+}
